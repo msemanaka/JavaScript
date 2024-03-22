23e4e1aa7a4dfb72ae28dcf64f52d1fd5144f01e
hunterblp <milena.sforte@gmail.com>
2024-03-22 10:32:42 -0300

    Atv for-ex02

---
+++ aula-04/for-ex02.js
@@ -0,0 +1,18 @@
+const leia = require('readline-sync');
+
+let numero, impar = 0, par = 0;
+
+console.log('CONTADOR DE NUMEROS PARES E IMPARES');
+
+for (contador = 1; contador <= 10; contador++) {
+    numero = leia.questionInt('Digite um numero: ');
+
+    if (numero % 2 === 0) {
+        par++;
+    } else {
+        impar++;
+    } 
+}
+
+console.log(`\nTotal de numeros pares: ${par}`);
+console.log(`Total de numeros impares: ${impar}`);
