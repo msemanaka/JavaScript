03498b1895dfa686c2cf92d72a4b48665d2ef406
hunterblp <milena.sforte@gmail.com>
2024-03-22 11:34:27 -0300

    Atv do-while-05

---
+++ aula-04/do-while-05.js
@@ -0,0 +1,15 @@
+const leia = require('readline-sync');
+
+let numero, soma = 0;
+
+console.log('Informe numeros inteiros positivos ou negativos, somente os positivos serão somados. Para "SAIR", digite 0.')
+
+do {
+    numero = leia.questionInt('Informe um numero: ');
+
+    if (numero > 0) {
+        soma = soma + numero; // ou (soma += numero)
+    }
+} while (numero !== 0)
+
+console.log(`A soma dos numeros positivos é: ${soma}`);
