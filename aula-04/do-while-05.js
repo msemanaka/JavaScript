41391ebe93ce74d8f8ec71146c67753a4bc7333f
hunterblp <milena.sforte@gmail.com>
2024-03-23 20:49:22 -0300

    formatação e add vetor-ex02

--- aula-04/do-while-05.js
+++ aula-04/do-while-05.js
@@ -2,10 +2,10 @@ const leia = require('readline-sync');
 
 let numero, soma = 0;
 
-console.log('Informe numeros inteiros positivos ou negativos, somente os positivos serão somados. Para "SAIR", digite 0.')
+console.log('\nInforme numeros inteiros positivos ou negativos, somente os positivos serão somados. Para "SAIR", digite 0.')
 
 do {
-    numero = leia.questionInt('Informe um numero: ');
+    numero = leia.questionInt('\nInforme um numero: ');
 
     if (numero > 0) {
         soma = soma + numero; // ou (soma += numero)
---
+++ aula-04/vetor-ex02.js
@@ -0,0 +1,34 @@
+const leia = require("readline-sync");
+
+let vetorInteiros = new Array(10);
+let soma = 0, media = 0;
+
+console.log('Digite 10 numeros inteiros: ');
+for (let indice = 0; indice < vetorInteiros.length; indice++) {
+    vetorInteiros[indice] = leia.questionInt(`Digite o numero ${indice + 1}: `);
+}
+
+console.log('\nElementos impares: ')
+for (indice = 0; indice < vetorInteiros.length; indice++) {
+    if (vetorInteiros[indice] % 2 !== 0) {
+        console.log(vetorInteiros[indice]);
+    }
+}
+
+console.log('\nElementos pares: ')
+for (indice = 0; indice < vetorInteiros.length; indice++) {
+    if (vetorInteiros[indice] % 2 === 0) {
+        console.log(vetorInteiros[indice]);
+    }
+}
+
+
+for (indice = 0; indice < vetorInteiros.length; indice++){
+    soma += vetorInteiros[indice]
+}
+console.log(`\nSoma de todos os numeros informados: ${soma}`)
+
+for (indice = 0; indice < vetorInteiros.length; indice++){
+    media = soma / vetorInteiros.length
+}
+console.log(`\nMedia: ${(media).toFixed(2)}`)
